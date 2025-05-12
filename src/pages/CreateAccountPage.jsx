ee989f70680743a887b7bbad0f99b6a51dca8cbe
Satya Chawan <chawansurya1106@gmail.com>
2025-05-12 22:28:32 +0530

    Updated full yogalayer project - frontend & backend

--- src/pages/CreateAccountPage.jsx
+++ src/pages/CreateAccountPage.jsx
@@ -17,7 +17,7 @@ export default function CreateAccountPage({ setUser, setCart, setWishlist }) {
   };
 
   const handleCreateAccount = async () => {
-    const apiUrl = "https://yogalayer-backend.onrender.com"; // TEMPORARY HARDCODE
+    const apiUrl = import.meta.env.VITE_API_BASE_URL;
     console.log("✅ API Base URL:", apiUrl);
 
     if (!apiUrl) {
