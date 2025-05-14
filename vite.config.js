b3961d8bc479e40b14d685195ac115508f561a44
Satya Chawan <chawansurya1106@gmail.com>
2025-05-14 20:08:47 +0530

    Updated full yogalayer project - frontend & backend

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000', // Redirect /api requests to the backend server
+    },
+  },
+});
